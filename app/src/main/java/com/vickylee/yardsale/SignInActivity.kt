3f7425b036f919f99db99fbbbaf62f4081c9f46d
Vicky Lee <ytleevicky@gmail.com>
2022-12-06 12:14:12 -0500

    Navigate to Sign up page

--- app/src/main/java/com/vickylee/yardsale/SignInActivity.kt
+++ app/src/main/java/com/vickylee/yardsale/SignInActivity.kt
@@ -1,11 +1,29 @@
 package com.vickylee.yardsale
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vickylee.yardsale.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    //region Properties
+    private lateinit var binding: ActivitySignInBinding
+
+    //endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        this.binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Sign up button
+        binding.btnSignUp.setOnClickListener {
+            // navigate to sign up screen
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
