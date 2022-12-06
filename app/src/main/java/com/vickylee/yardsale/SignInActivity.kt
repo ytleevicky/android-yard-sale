5dffbfa914ec35af1be58a2a257aaf26f95adde5
Vicky Lee <ytleevicky@gmail.com>
2022-12-06 13:14:18 -0500

    Create bottom navigation

--- app/src/main/java/com/vickylee/yardsale/SignInActivity.kt
+++ app/src/main/java/com/vickylee/yardsale/SignInActivity.kt
@@ -8,22 +8,31 @@ import com.vickylee.yardsale.databinding.ActivitySignInBinding
 class SignInActivity : AppCompatActivity() {
 
     //region Properties
+    val TAG = this@SignInActivity.toString()
     private lateinit var binding: ActivitySignInBinding
-
     //endregion
 
+    //region Android Lifecycle Methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // setup view binding
         this.binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // sign in button
+        binding.btnSignIn.setOnClickListener {
+            // TODO: check user type -> Seller or Buyer
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
-        // Sign up button
+        // sign up button
         binding.btnSignUp.setOnClickListener {
             // navigate to sign up screen
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-
     }
+    //endregion
 }
--- app/src/main/java/com/vickylee/yardsale/fragments/ListViewFragment.kt
+++ app/src/main/java/com/vickylee/yardsale/fragments/ListViewFragment.kt
@@ -3,6 +3,6 @@ package com.vickylee.yardsale.fragments
 import androidx.fragment.app.Fragment
 import com.vickylee.yardsale.R
 
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
+class ListViewFragment : Fragment(R.layout.fragment_list_view) {
 
 }
